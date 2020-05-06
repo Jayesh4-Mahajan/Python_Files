5856c80222647bfe7011a98d270e4ea7b8437a68
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2020-05-06 17:12:22 +0530

    Update log_sql_template.py

--- log_sql_template.py
+++ log_sql_template.py
@@ -9,7 +9,6 @@ dir_name=os.path.dirname(os.path.abspath(__file__))
 file_name=os.path.basename(sys.argv[0])
 table_name=file_name.split('.')[0]
 log_file=dir_name+'/'+table_name+'_log.txt'
-sql_file=dir_name+'/'+table_name+'_sql.txt'
 
 ##### Loading current datetime #####
 curr_dt=strftime("%d-%m-%Y %H:%M:%S", gmtime())
@@ -21,14 +20,6 @@ log_f.write("######################################################")
 log_f.write("\n")
 log_f.write(curr_dt+":"+ table_name +" loaded")
 log_f.write("\n")
-
-###### Loading and substituting variables in the sql file ######
-file1=open(sql_file,'r+', encoding="utf-8")
-sql_raw = s = " ".join(file1.readlines())
-sql_raw=sql_raw.replace('\t',' ')
-sql_raw=sql_raw.replace('\n',' ')
-t=string.Template(sql_raw)
-sql=t.substitute(vars())
 
 ### Running the required script #######
 try:
