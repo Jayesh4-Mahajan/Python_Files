10665b55279e2d386d94243c5547617a938f4468
primo04 <53470246+Jayesh4-Mahajan@users.noreply.github.com>
2022-12-01 13:00:53 +0530

    Create read_josn.py

---
+++ read_josn.py
@@ -0,0 +1,22 @@
+
+# Python program to read
+# json file
+# Source: https://www.geeksforgeeks.org/read-json-file-using-python/
+  
+  
+import json
+  
+# Opening JSON file
+f = open('data.json')
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+for i in data['emp_details']:
+    print(i)
+  
+# Closing file
+f.close()
